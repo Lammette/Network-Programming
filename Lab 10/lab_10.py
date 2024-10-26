112b2d571774423a333aac96028492d989cdb56e
Lammet <leonlammetteam123@gmail.com>
2024-10-26 16:10:31 +0200

    asdasdadadaddsdsdasdasd

--- Lab 10/lab_10.py
+++ Lab 10/lab_10.py
@@ -2,4 +2,11 @@ import re
 f = open(r"Lab 10\tabla.html",encoding="utf-8")
 txt = f.read()
 
-print(re.findall(r'<td class="svtTablaTime">\s*(\d+\.\d+)\s*</td>', txt))
+tabla = re.findall(r'<td class="svtTablaTime">\s*(\d+\d.\d+)\s*<\/td>\s*<td.*?>\s*<h4.*?>\s*Simpsons\s*<\/h4>\s*.*\s*.* \s*.*\s*.*\s*.*\s*.*?[.]\D*(\d+)\D*(\d+)\D*(\d+)\W*(.*?)[.]', txt )
+
+for i in range(0,len(tabla)):
+    print("--------------------")
+    print(f"Tid:\t{tabla[i][0]}")
+    print(f"Säsong:\t{tabla[i][1]}")
+    print(f"Avsnitt:{tabla[i][2]}/{tabla[i][3]}")
+    print(f"Handling: {tabla[i][4]}")
