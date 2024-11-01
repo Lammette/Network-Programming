b78309d24971ad265498249ebfc4bba814f1401c
Lammette <leonlammetteam123@gmail.com>
2024-11-01 13:20:27 +0100

    addadadsdsdsdadadaeqqeeqewewweqe

--- Lab 11A/lab_11a.py
+++ Lab 11A/lab_11a.py
@@ -1,7 +1,10 @@
 import urllib.request
 import re
 
-req = urllib.request.Request('https://www.svtplay.se/kanaler?date=2020-11-01')
+# "(ch-\w*).*?schedule.*?\[(.*?)\] Get channel schedule
+#{\\"descriptionRaw\\":\\"(?:(.*?))\\",\\"name\W*(.*?)\\.*?subHeading\\":\\"(?:(.*?))\\.*?startTime.*?(\d{2}:\d{2}).*?start.*?(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}) #get programs
+
+req = urllib.request.Request('https://www.svtplay.se/kanaler?date=2024-10-31')
 with urllib.request.urlopen(req) as response:
    data = response.read().decode("utf-8")
    tabla = re.findall(r'<td class="svtTablaTime">\s*(\d+\d.\d+)\s*<\/td>\s*<td.*?>\s*<h4.*?>\s*Simpsons\s*<\/h4>\s*.*\s*.* \s*.*\s*.*\s*.*\s*.*?[.]\D*(\d+)\D*(\d+)\D*(\d+)\W*(.*?)[.]', data )
